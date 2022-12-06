4b25d58003cb024cec649bb0f8b84c7afde71325
gracewzhang <gracewzhang01@gmail.com>
2022-12-06 16:17:58 -0600

    Set up client structure

---
+++ client/src/components/Thoughts/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { HeartIcon, CheckIcon } from '@heroicons/react/outline';
+
+const Thoughts = (): React.ReactElement => {
+  return (
+    <div>
+      Goodbye
+    </div>
+  );
+};
+
+export default Thoughts;
