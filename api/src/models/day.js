da14dc202059a98161a77e4f164a78f4b208e8d5
gracewzhang <gracewzhang01@gmail.com>
2022-12-06 21:56:03 -0600

    Fix db call

--- api/src/models/day.js
+++ api/src/models/day.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const Day = new mongoose.Schema({
-  // user: { type: mongoose.Schema.Types.ObjectId }, // add the ref: 'User' thing
-  date: { type: String, default: "" },
-  title: { type: String, default: "" },
-  entry: { type: String, default: "" },
-  thoughts: { type: [String], default: ["", "", ""] },
-  photos: { type: [String], default: ["", "", "", ""] },
-  is_favorite: { type: Boolean, default: false },
-});
+const Day = new mongoose.Schema(
+  {
+    // user: { type: mongoose.Schema.Types.ObjectId }, // add the ref: 'User' thing
+    date: { type: String, default: "" },
+    title: { type: String, default: "" },
+    entry: { type: String, default: "" },
+    thoughts: { type: [String], default: ["", "", ""] },
+    photos: { type: [String], default: ["", "", "", ""] },
+    is_favorite: { type: Boolean, default: false },
+  },
+  { collection: "days" }
+);
 
 module.exports = mongoose.model("Day", Day);
