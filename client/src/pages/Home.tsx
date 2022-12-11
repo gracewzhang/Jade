07b850502c06b92bfbbcebbfc1b4fd085d88fc31
gracewzhang <gracewzhang01@gmail.com>
2022-12-11 17:04:30 -0600

    Crying the auth is working

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { useAuthContext } from '../contexts/AuthContext';
 
 const Home = (): React.ReactElement => {
+  // TODO: const [user, setUser] = useLocalStorage("user", null);
   const { user } = useAuthContext();
+  console.log(user);
   return (
     <>
-      <p>{user?.name ?? 'oh'}</p>
+      <p>{user[0]?.name ?? 'oh'}</p>
     </>
   );
 };
