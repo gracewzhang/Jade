a59d85c86b24fe33a3f7042f7ae57a5ca5a7f645
gracewzhang <gracewzhang01@gmail.com>
2022-12-16 09:47:47 -0800

    Add toast

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { useAuthContext } from './contexts/AuthContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import RequireAuth from './components/RequireAuth/RequireAuth';
 import Home from './pages/Home';
 import Login from './pages/Login';
 
+const StyledToastContainer = styled(ToastContainer)`
+  --toastify-color-success: #FFB2A7;
+  --toastify-color-error: #e74c3c;
+  --toastify-font-family: 'Ubuntu', sans-serif;
+  --toastify-text-color-info: #ffff;
+  --toastify-text-color-success: #ffff;
+  --toastify-text-color-warning: #ffff;
+  --toastify-text-color-error: #ffff;
+`;
+
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Ubuntu', sans-serif;
+  }
+
+  h1 {
+    font-size: 2.3em;
+    margin: 0;
+    align-items: center;
+    display: flex;
   }
 `;
 
@@ -23,6 +42,7 @@ const App = (): React.ReactElement => {
     <>
       <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID ?? ''}>
         <GlobalStyle />
+        <StyledToastContainer/>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<RequireAuth><Home /></RequireAuth>} />
--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -6,14 +6,19 @@ const LogoContainer = styled.span`
   display: flex;
 `;
 
+const StyledLogoIllustration = styled(LogoIllustration)`
+  width: 70px;
+`;
+
 const Title = styled.h1`
+  padding-left: 5%;
 `;
 
 const Logo = (props: any): JSX.Element => {
   const { className } = props;
   return (
     <LogoContainer className={className}>
-      <LogoIllustration/>
+      <StyledLogoIllustration/>
       <Title>Jade</Title>
     </LogoContainer>
   );
