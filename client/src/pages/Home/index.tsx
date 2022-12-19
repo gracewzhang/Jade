74b4a4ee46e042a3fc7af6c5748a867f9f8fa31b
gracewzhang <gracewzhang01@gmail.com>
2022-12-19 11:32:32 -0800

    Set up home layout

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -11,13 +11,45 @@ const HomeContainer = styled.div`
   padding: 5%;
 `;
 
+const ContentContainer = styled.span`
+  display: grid;
+  grid-template-columns: 60% 40%;
+`;
+
+const LeftContentContainer = styled.div`
+  display: grid;
+  grid-template-rows: 40% 60%;
+`;
+
+const BottomContentContainer = styled.span`
+  display: grid;
+  grid-template-columns: 60% 40%;
+`;
+
+const RightContentContainer = styled.div`
+  display: grid;
+  grid-template-rows: 70% 30%;
+`;
+
 const Home = (): React.ReactElement => {
   // TODO: const [user, setUser] = useLocalStorage("user", null);
   const { user } = useAuthContext();
   return (
     <HomeContainer>
       <Welcome/>
-      <p>yo</p>
+      <ContentContainer>
+        <LeftContentContainer>
+          <p>hello</p>
+          <BottomContentContainer>
+            <p>hello 2</p>
+            <p>hello 3</p>
+          </BottomContentContainer>
+        </LeftContentContainer>
+        <RightContentContainer>
+          <p>fdjls</p>
+          <p>fjdks;lafjdlks;</p>
+        </RightContentContainer>
+      </ContentContainer>
     </HomeContainer>
   );
 };
