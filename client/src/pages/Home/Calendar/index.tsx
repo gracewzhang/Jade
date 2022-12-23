ca05ac0ec7f3589f77af016a8b7af02d39d96d93
Grace Zhang <gracewzhang01@gmail.com>
2022-12-23 09:51:32 -0800

    Fix heights but all of the css is messy

--- client/src/pages/Home/Calendar/index.tsx
+++ client/src/pages/Home/Calendar/index.tsx
@@ -10,13 +10,45 @@ const CalendarContainer = styled(Block)``;
 
 const StyledCalendar = styled(_Calendar)`
   border: 0;
+  border-radius: 30px;
   width: 100%;
-  padding-left: 30px;
-  padding-right: 30px;
+  height: 100%;
+  padding: 30px;
   font-family: 'Ubuntu', sans-serif;
+
+  .react-calendar__tile {
+    border-radius: 30px;
+  }
 
   button {
     font-family: 'Ubuntu', sans-serif;
+  }
+
+  .react-calendar__navigation {
+    height: 10%;
+    margin-bottom: 5%;
+  }
+
+  .react-calendar__viewContainer {
+    height: 90%;
+  }
+
+  .react-calendar__month-view {
+    height: 100%;
+
+    > div {
+      height: 100%;
+
+      > div {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+      }
+    }
+  }
+
+  .react-calendar__month-view__days {
+    height: 100%;
   }
 
   .react-calendar__month-view__days__day--weekend {
@@ -27,8 +59,18 @@ const StyledCalendar = styled(_Calendar)`
     color: grey; // TODO
   }
 
+  .react-calendar__navigation button {
+    border-radius: 30px;
+  }
+
   .react-calendar__tile:disabled {
-    color: grey; // TODO
+    color: lightgrey; // TODO
+    background-color: white; // TODO
+  }
+
+  .react-calendar__navigation button:disabled {
+    color: lightgrey;
+    background-color: white;
   }
 
   .react-calendar__tile--active {
