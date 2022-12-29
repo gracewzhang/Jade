9b24e1a630ab7f2bb769cbb8067f63ef397a7688
Grace Zhang <gracewzhang01@gmail.com>
2022-12-29 11:46:11 -0800

    Set up photos frontend

--- client/src/pages/Home/Photos/index.tsx
+++ client/src/pages/Home/Photos/index.tsx
@@ -1,16 +1,66 @@
 import React from 'react';
 import styled from 'styled-components';
+import Dropzone from 'react-dropzone';
+import { HiOutlinePlus } from 'react-icons/hi2';
 
 import Block from '../../../components/Block';
+import colors from '../../../utils/colors';
 
 const PhotosContainer = styled(Block)`
+  display: flex;
+  flex-direction: row;
 
+  > div + div {
+    border-left: 1px dashed ${colors['light-grey']};
+  }
 `;
+
+const PhotoContainer = styled.div`
+  width: 12.5vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const StyledDropzone = styled(Dropzone)``;
+
+const StyledPlusIcon = styled(HiOutlinePlus)`
+  color: ${colors.rose};
+  width: 40px;
+  height: 40px;
+  stroke-width: 3px;
+`;
+
+const Photo = (): React.ReactElement => {
+  return (
+    <PhotoContainer>
+      <StyledDropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>
+                <StyledPlusIcon />
+              </p>
+            </div>
+          </section>
+        )}
+      </StyledDropzone>
+    </PhotoContainer>
+  );
+};
 
 const Photos = (): React.ReactElement => {
   return (
     <PhotosContainer>
-      Photos
+      <Photo />
+      <Photo />
+      <Photo />
+      <Photo />
     </PhotosContainer>
   );
 };
