85cfca7d1d5c40333914f2f3c34efccac1ce896c
Grace Zhang <gracewzhang01@gmail.com>
2022-12-30 09:54:42 -0800

    Uploading files works yayyyyyy

--- client/src/pages/Home/Photos/index.tsx
+++ client/src/pages/Home/Photos/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Dropzone from 'react-dropzone';
 import { HiOutlinePlus } from 'react-icons/hi2';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 
 import Block from '../../../components/Block';
 import colors from '../../../utils/colors';
+import storage from '../../../utils/firebase';
 
 const PhotosContainer = styled(Block)`
   display: flex;
@@ -44,13 +46,41 @@ const StyledPlusIcon = styled(HiOutlinePlus)`
 `;
 
 const Photo = (): React.ReactElement => {
+  const [percent, setPercent] = useState(0);
+
+  const handleUpload = (acceptedFiles: File[]): void => {
+    const file = acceptedFiles[0];
+    console.log(file);
+    const storageRef = ref(storage, `/files/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const percent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        ); // update progress
+        setPercent(percent);
+      },
+      (err) => console.log(err),
+      () => {
+        // download url
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          console.log(url);
+        });
+      }
+    );
+  };
+
   return (
     <PhotoContainer>
-      <StyledDropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+      <StyledDropzone maxFiles={1} onDrop={handleUpload}>
         {({ getRootProps, getInputProps }) => (
           <StyledDropzoneDiv {...getRootProps()}>
             <input {...getInputProps()} />
             <StyledPlusIcon />
+            <p>{percent}</p>
           </StyledDropzoneDiv>
         )}
       </StyledDropzone>
