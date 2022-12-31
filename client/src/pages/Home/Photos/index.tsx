73b419c73472b04dd5ee9d51ae933050e199aa12
Grace Zhang <gracewzhang01@gmail.com>
2022-12-31 11:27:14 -0800

    Prevent images from resizing

--- client/src/pages/Home/Photos/index.tsx
+++ client/src/pages/Home/Photos/index.tsx
@@ -52,6 +52,7 @@ const StyledPlusIcon = styled(HiOutlinePlus)`
 const StyledImg = styled.img`
   width: 100%;
   height: 100%;
+  object-fit: cover;
 `;
 
 const Photo = (props: PhotoProps): React.ReactElement => {
