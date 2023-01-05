305e9b2ee8881a303f5ea8a6d5f8cd360a38865c
Grace Zhang <gracewzhang01@gmail.com>
2023-01-05 15:28:14 -0800

    Set up padding & skeleton

--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,52 +1,61 @@
 import React from 'react';
 import styled from 'styled-components';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+
 import Gallery from './Gallery';
 import Header from './Header';
 import Settings from './Settings';
 import Statistics from './Statistics';
+import colors from '../../utils/colors';
 
 const ProfileContainer = styled.div`
   display: grid;
-  grid-template-rows: 20% 80%;
+  grid-template-rows: 13vh 71vh;
   padding: 7vh 7vw 9vh 7vw;
   height: 84vh;
 `;
 
-const BottomContainer = styled.span`
+const ContentContainer = styled.span`
   display: grid;
-  grid-template-columns: 40% 50%;
+  grid-template-columns: 50% 50%;
 `;
 
 const LeftContentContainer = styled.div`
   display: grid;
-  grid-template-rows: 40% 50%;
+  grid-template-rows: 36vh 35vh;
+  padding-right: 5vw;
 `;
 
-const StatisticsContainer = styled.div``;
+const StatisticsContainer = styled.div`
+  padding-bottom: 8vh;
+`;
 
 const SettingsContainer = styled.div``;
 
 const GalleryContainer = styled.div``;
 
-// TODO: loading skeleton
 const Profile = (): React.ReactElement => {
+  const loading = false; // TODO
+
   return (
-    <ProfileContainer>
-      <Header />
-      <BottomContainer>
-        <LeftContentContainer>
-          <StatisticsContainer>
-            <Statistics />
-          </StatisticsContainer>
-          <SettingsContainer>
-            <Settings />
-          </SettingsContainer>
-        </LeftContentContainer>
-        <GalleryContainer>
-          <Gallery />
-        </GalleryContainer>
-      </BottomContainer>
-    </ProfileContainer>
+    <SkeletonTheme baseColor={colors['super-light-grey']} borderRadius="30px">
+      <ProfileContainer>
+        <Header />
+        <ContentContainer>
+          <LeftContentContainer>
+            <StatisticsContainer>
+              {loading ? <Skeleton count={13} /> : <Statistics />}
+            </StatisticsContainer>
+            <SettingsContainer>
+              {loading ? <Skeleton count={15} /> : <Settings />}
+            </SettingsContainer>
+          </LeftContentContainer>
+          <GalleryContainer>
+            {loading ? <Skeleton count={31} /> : <Gallery />}
+          </GalleryContainer>
+        </ContentContainer>
+      </ProfileContainer>
+    </SkeletonTheme>
   );
 };
 
