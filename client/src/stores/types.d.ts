08227b3a51974fd6c7c70122fc4a7ece50842604
Grace Zhang <gracewzhang01@gmail.com>
2023-01-09 14:33:32 -0800

    Migrate login

--- client/src/stores/types.d.ts
+++ client/src/stores/types.d.ts
@@ -1,11 +1,11 @@
-import { User } from '../types/user';
+import { AuthUser, User } from '../types/user';
 
 export interface StoreResults {
   user: undefined | User;
   setUser: (newUser: User) => void;
   checkIfNewUser: (googleId: string) => Promise<boolean>;
-  signIn: (data: User) => Promise<void>;
-  signUp: (data: User) => Promise<void>;
+  signIn: (data: AuthUser) => Promise<void>;
+  signUp: (data: AuthUser) => Promise<void>;
   signOut: () => void;
   updateUser: (props: UpdateUserProps) => Promise<void>;
 }
