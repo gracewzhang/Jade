9298e2bcca7b334c4c88c3ded2e09183ad37ef83
Grace Zhang <gracewzhang01@gmail.com>
2023-01-11 18:45:40 -0800

    Styled gallery grid

--- client/src/pages/Profile/Gallery/index.tsx
+++ client/src/pages/Profile/Gallery/index.tsx
@@ -8,11 +8,20 @@ import useStore from '../../../stores';
 import { PhotoProps } from './types';
 
 // TODO: cmd shift f all of the `` to <>
-const GalleryContainer = styled(Block)``;
+const GalleryContainer = styled(Block)`
+  display: grid;
+  grid: repeat(5, 20%) / repeat(5, 1fr);
+  align-content: start;
+  gap: 0.5em;
+  overflow-y: scroll;
+  grid-auto-rows: 20%;
+  grid-auto-columns: 20%;
+`;
 
 const StyledImg = styled.img`
   width: 100%;
   height: 100%;
+  object-fit: cover;
 `;
 
 const Photo = (props: PhotoProps): React.ReactElement => {
@@ -23,6 +32,7 @@ const Photo = (props: PhotoProps): React.ReactElement => {
 
 const FIREBASE_ROOT = process.env.REACT_FIREBASE_ROOT ?? 'test';
 
+// TODO: photo uploading is buggy
 const Gallery = (): React.ReactElement => {
   const user = useStore((state) => state.user);
   const [urls, setUrls] = useState<string[]>([]);
@@ -40,7 +50,6 @@ const Gallery = (): React.ReactElement => {
         .catch((err) => {
           console.log(err);
         });
-
       setUrls(photoUrls);
     }
   };
@@ -50,11 +59,14 @@ const Gallery = (): React.ReactElement => {
     retrievePhotos();
   }, []);
 
+  const loading = false; // TODO: skeleton
+
   return (
     <GalleryContainer>
       {urls.map((url, key) => (
         <Photo url={url} key={key} />
       ))}
+      {/* {urls.length > 25 && <ViewAllButton>View All</ViewAllButton>} */}
     </GalleryContainer>
   );
 };
